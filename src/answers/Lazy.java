6b77cfb07345bed03904906bccd9417dfdda0394
Oleksandra_Dmytrenko <Oleksandra_Dmytrenko1@epam.com>
2016-03-10 19:07:23 +0200

    Unboxing fix

--- src/answers/Lazy.java
+++ src/answers/Lazy.java
@@ -10,17 +10,35 @@ import java.util.function.Supplier;
 public class Lazy {
 
     public <R> R use(Supplier<R> supplier) {
-
-        return supplier.get();
+        System.out.println("Start use");
+        R r = supplier.get();
+        System.out.println("End use r=" + r);
+        return r;
     }
 
-    public String getString(){
+    public String getString() {
+        System.out.println("Start getString");
+        use(() -> "I'm use implementation in getString");
+        System.out.println("End getString");
+        return "I'm get String return";
+    }
+
+    public String getStringUse() {
 
         return use(() -> "I'm use implementation in getString");
     }
 
+    public void getStringVoid() {
+        System.out.println("Start getStringVoid");
+        System.out.println(use(() -> "I'm use implementation in getString"));
+        System.out.println("End getStringVoid");
+    }
+
     @Test
-    public void testGetString(){
+    public void testGetString() {
         System.out.println(getString());
+        // System.out.println(getStringUse());
+        // getStringVoid();
+
     }
 }
